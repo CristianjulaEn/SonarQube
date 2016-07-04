754c9fed303b6d7aa386745d74ae083d4d50522a
CruzanCaramele <letters2hamza@gmail.com>
2016-07-04 22:47:33 +0800

    accept the oracle license

--- scripts/programs.sh
+++ scripts/programs.sh
@@ -1,7 +1,13 @@
 #bin/bash -eux
 
+#accept the oracle license
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+
 #Add the WebUpd8 Team Personal Package Archive 
 sudo apt-add-repository ppa:webupd8team/java
 
 #update system packages
 sudo apt-get update
+
+#install Oracle Java 8 with the PPA installer
+sudo apt-get install oracle-java8-installer
