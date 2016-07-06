d9cc4a32267203509f070940d74838f965713518
CruzanCaramele <letters2hamza@gmail.com>
2016-07-06 17:47:07 +0800

    define public subnet and its properties

--- Server/vpc.tf
+++ Server/vpc.tf
@@ -25,3 +25,14 @@ resource "aws_internet_gateway" "sonar_gateway" {
 		create_before_destroy = true
 	}
 }
+
+#--------------------------------------------------------------
+# Public Subnet
+#--------------------------------------------------------------
+resource "aws_subnet" "sonar_public_subnet" {
+	vpc_id                 = "${aws_vpc.sonar_vpc.id}"
+	cidr_block             = "${element(split(",", var.public_cidrs), count.index)}"
+	availability_zone      = "${element(split(",", var.azs), count.index)}"
+	count                  = "${length(split(",", var.cidrs))}"
+	map_public_ip_on_lunch = true
+}
