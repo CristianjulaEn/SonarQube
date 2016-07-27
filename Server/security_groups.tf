005bb6c8c729c66e23046374984942111ee384fc
CruzanCaramele <letters2hamza@gmail.com>
2016-07-27 22:51:36 +0800

    add ingress traffic for tcp ports for www on web sg

--- Server/security_groups.tf
+++ Server/security_groups.tf
@@ -81,4 +81,11 @@ resource "aws_security_group" "web" {
 		protocol    = "tcp"
 		cidr_blocks = ["0.0.0.0/0"]
 	}
+
+	ingress {
+		from_port   = 443
+		to_port     = 443
+		protocol    = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
 }
