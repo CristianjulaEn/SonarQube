ec1528705b240754fa4ff11793925474c4173ec9
CruzanCaramele <letters2hamza@gmail.com>
2016-07-28 21:55:14 +0800

    remove provider and move to vpc file

--- Server/variables.tf
+++ Server/variables.tf
@@ -17,31 +17,32 @@ variable "aws_secret_key" {
 # Access and Secret Keys
 #--------------------------------------------------------------
 variable "region" {
-  description = "The AWS region to create resources in."
-  default = "us-east-1"
+	type        = "string"
+	description = "The AWS region to create resources in."
+  	default     = "us-east-1"
 }
 
 #--------------------------------------------------------------
 # Availability Zones
 #--------------------------------------------------------------
 variable "azs" {
-	type = "string"
+	type        = "string"
 	description = "Availability Zones"
-	default = "us-east-1a,us-east-1a,us-east-1a"
+	default     = "us-east-1a,us-east-1a,us-east-1a"
 }
 
 #--------------------------------------------------------------
 # Public & Private CIDRs
 #--------------------------------------------------------------
 variable "public_cidrs" {
-	type = "string"
+	type        = "string"
 	description = "public network blocks"
-	default = "172.31.0.0/20,172.31.16.0/20,172.31.32.0/20"
+	default     = "172.31.0.0/20,172.31.16.0/20,172.31.32.0/20"
 }
 
 variable "private_cidrs" {
-	type = "string"
+	type        = "string"
 	description = "private network blocks"
-	default = "172.31.48.0/20,172.31.64.0/20,172.31.80.0/20"
+	default     = "172.31.48.0/20,172.31.64.0/20,172.31.80.0/20"
 }
 
