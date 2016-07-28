ebf0e214eaf6347ba3b67c6296e47fc282274022
CruzanCaramele <letters2hamza@gmail.com>
2016-07-28 23:34:56 +0800

    exclude avail zones

--- Server/instances.tf
+++ Server/instances.tf
@@ -6,3 +6,20 @@ resource "atlas_artifact" "SonarQube" {
 	version = "latest"
 	type    = "amazon.ami"
 }
+
+#--------------------------------------------------------------
+# Instance(s)
+#--------------------------------------------------------------
+resource "aws_instance" "Sonar-Server" {
+	ami             = "${atlas_artifact.SonarQube.metadata_full.ami_id}"
+	instance_type   = "c3.large"
+	subnet_id       = "${element(aws_subnet.sonar_private_subnet.*.id, count.index)}"
+	security_groups = ["${aws_security_group.primary.id}"]
+	depends_on      = ["aws_internet_gateway.sonar_gateway"]
+	count           = 3
+
+	lifecycle {
+		create_before_destroy = true
+	}
+}
+
