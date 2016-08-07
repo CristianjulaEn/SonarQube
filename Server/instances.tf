aa819a4118d15927a476d4b278e4532abed496eb
CruzanCaramele <letters2hamza@gmail.com>
2016-08-07 15:20:56 +0800

    add user connection from bastion instance

--- Server/instances.tf
+++ Server/instances.tf
@@ -1,23 +1,38 @@
 #--------------------------------------------------------------
 # Atlas Artifact (AMI)
 #--------------------------------------------------------------
-resource "atlas_artifact" "SonarQube" {
+data "atlas_artifact" "SonarQube" {
 	name    = "Panda/SonarQube"
 	version = "latest"
-	type    = "amazon.ami"
-}
+	type    = "amazon.image"
+}	
 
 #--------------------------------------------------------------
 # Instance(s)
 #--------------------------------------------------------------
 resource "aws_instance" "Sonar-Server" {
-	ami             = "${atlas_artifact.SonarQube.metadata_full.ami_id}"
+	ami             = "${data.atlas_artifact.SonarQube.metadata_full.region-us-east-1}"
 	instance_type   = "c3.large"
 	subnet_id       = "${element(aws_subnet.sonar_private_subnet.*.id, count.index)}"
 	security_groups = ["${aws_security_group.primary.id}"]
 	depends_on      = ["aws_internet_gateway.sonar_gateway"]
+	key_name        = "${aws_key_pair.SonarKey.key_name}"
+	monitoring      = true
 	count           = 3
 
+	tags {
+		Name       = "Sonar-Server"
+		role       = "app-server"
+		monitoring = ""
+	}
+
+	connection {
+		user         = "ubuntu"
+		key_file     = "${var.key_file}"
+		bastion_host = "${aws_eip.BastionEip.public_ip}"
+		agent        = false
+	}
+	
 	lifecycle {
 		create_before_destroy = true
 	}
